Fixed sigma calculation and added elegant efficiency gain

packages/mixture/GaussianMixture.py
@@ -9,10 +9,12 @@ def _initialize_assignments(X, K):
     N = len(X)
     A_T = np.zeros((K, N))
 
-    for k in range(K):
+    for k in range(K-1):
         # generate random probabilities for each sample
         p_ck = np.random.uniform(size=N)
         A_T[k] = p_ck
+
+    # final kth cluster derived so every row adds up to 1.0
 
     return A_T.T
 

packages/mixture/expectation_maximization.py
@@ -43,7 +43,6 @@ def _calculate_all_pi(A, S):
 
 
 # tested
-# ?? seems odd to get mu as a vector (not a scalar)
 def _calculate_mu_k(X, A, S, k):
     """
     Calculates mu parameter for cluster k. Uses the following formula for mu_k:
@@ -65,35 +64,11 @@ def _calculate_mu_k(X, A, S, k):
     """
     a_k = A[:, k].reshape(-1, 1)  # soft assignments for kth cluster
     s_k = S[k]  # sum of soft assignments for kth cluster
-    return np.sum(X * a_k, axis=0) / s_k
+    mu_k = np.sum(X * a_k, axis=0) / s_k
+    return mu_k
 
 
 # tested
-def _calculate_X_corr(X, mu_k):
-    """
-    (X^n - mu_k)(X^n - mu_k)^T
-
-    This implementation takes advantage of the fact that (X^n - mu_k)(X^n - mu_k)^T is the same as squaring each
-    dimension of the row vector then adding all terms together:
-
-    Example:
-    Assume (X^n - mu_k) = | 1.7 | -0.3 |
-
-    | 1.7 | -0.3 |  x  |  1.7 |  = (1.7 * 1.7) + (-0.3 * -0.3) = 2.98
-                       | -0.3 |
-
-    The function does this for all X^n in parallel.
-
-    :param X:
-    :param mu_k:
-    :return:
-    """
-    X_corr = X - mu_k
-    return np.sum(np.square(X_corr), axis=1).reshape(-1, 1)
-
-
-# tested
-# ?? seems odd to get sigma as a scalar (not a matrix)
 def _calculate_sigma_k(X, A, S, mu_k, k):
     """
     Calculates Sigma parameter for cluster k. Uses the following formula for sigma_k:
@@ -115,108 +90,105 @@ def _calculate_sigma_k(X, A, S, mu_k, k):
     :param k: kth cluster, represents cluster for which parameter is being calculated
     :return: N x N matrix
     """
-    X_corr = _calculate_X_corr(X, mu_k)
+    X_minus_mu_k = X - mu_k
     a_k = A[:, k].reshape(-1, 1)
     s_k = S[k]
+    return np.matmul((a_k * X_minus_mu_k).T, X_minus_mu_k) / s_k
 
-    sigma_k = np.sum(X_corr * a_k, axis=0) / s_k
-
-    return sigma_k.item(0)
-
-
-# tested
-def m_step(X, A):
-    """
-    Performs MLE for parameter estimation using data and soft assignments.
-
-    :param X: N x J matrix, where N is the number of samples and J is the number of features per sample
-    :param A: N x K matrix, where K is the number of clusters
-    :return: (list, list, list) Tuple representing ( pi, mu, sigma) parameters for all K clusters.
-    """
-    S = _sum_soft_assignments(A)  # used multiple times so calculated separately
-    K = len(S)
-
-    all_pi = _calculate_all_pi(A, S)
-    all_mu = []
-    all_sigma = []
-
-    for k in range(K):
-        mu_k = _calculate_mu_k(X, A, S, k)
-        all_mu.append(mu_k)
-
-        sigma_k = _calculate_sigma_k(X, A, S, mu_k, k)
-        all_sigma.append(sigma_k)
-
-    return all_pi, all_mu, all_sigma
-
-
-# tested
-# ?? Do I sum up over all X^n here?
-def _calculate_prob_Xk(X, all_pi, all_mu, all_sigma, k):
-    mu_k = all_mu[k]
-    sigma_k = all_sigma[k]
-    pi_k = all_pi[k]
-
-    y = multivariate_normal.pdf(X, mean=mu_k, cov=sigma_k)
-    p_Xk = y * pi_k
-    return p_Xk.reshape(-1, 1)
-
-
-# tested
-def _calculate_prob_X(X, all_pi, all_mu, all_sigma):
-    """
-    Calculates P(X), the denominator in the inference equation. Uses the following formula:
-
-    P(X^n) = SUM_k=1^K p(X^n|c^n=k) * p(c^n=k)
-
-    :param X:
-    :param all_pi:
-    :param all_mu:
-    :param all_sigma:
-    :return:
-    """
-    N = len(X)
-    K = len(all_pi)
-    all_prob = np.zeros((N, K))
-
-    for k in range(K):
-        prob_Xk = _calculate_prob_Xk(X, all_pi, all_mu, all_sigma, k)
-        all_prob[:, k] = prob_Xk[:, 0]
-    p_X = np.sum(all_prob, axis=1).reshape(-1, 1)
-    return p_X
-
-
-# tested
-def calculate_log_likelihood(X, all_pi, all_mu, all_sigma):
-    p_X = _calculate_prob_X(X, all_pi, all_mu, all_sigma)
-    log_p_X = np.log(p_X)
-    sum_p_X = np.sum(log_p_X,axis=0)
-    return sum_p_X.item(0)
-
-
-# tested
-def e_step(X, all_pi, all_mu, all_sigma):
-    """
-    Infers soft assignments to each cluster for each sample.
-
-    :param X:
-    :param all_pi:
-    :param all_mu:
-    :param all_sigma:
-    :return:
-    """
-    p_X = _calculate_prob_X(X, all_pi, all_mu, all_sigma)
-
-    N = len(X)
-    K = len(all_pi)
-    A = np.zeros((N, K))
-
-    for k in range(K):
-        p_Xk = _calculate_prob_Xk(X, all_pi, all_mu, all_sigma, k)
-        a_k = p_Xk / p_X
-        A[:, k] = a_k[:, 0]
-
-    return A
+#
+# # tested
+# def m_step(X, A):
+#     """
+#     Performs MLE for parameter estimation using data and soft assignments.
+#
+#     :param X: N x J matrix, where N is the number of samples and J is the number of features per sample
+#     :param A: N x K matrix, where K is the number of clusters
+#     :return: (list, list, list) Tuple representing ( pi, mu, sigma) parameters for all K clusters.
+#     """
+#     S = _sum_soft_assignments(A)  # used multiple times so calculated separately
+#     K = len(S)
+#
+#     all_pi = _calculate_all_pi(A, S)
+#     all_mu = []
+#     all_sigma = []
+#
+#     for k in range(K):
+#         mu_k = _calculate_mu_k(X, A, S, k)
+#         all_mu.append(mu_k)
+#
+#         sigma_k = _calculate_sigma_k(X, A, S, mu_k, k)
+#         all_sigma.append(sigma_k)
+#
+#     return all_pi, all_mu, all_sigma
+#
+#
+# # tested
+# # ?? Do I sum up over all X^n here?
+# def _calculate_prob_Xk(X, all_pi, all_mu, all_sigma, k):
+#     mu_k = all_mu[k]
+#     sigma_k = all_sigma[k]
+#     pi_k = all_pi[k]
+#
+#     y = multivariate_normal.pdf(X, mean=mu_k, cov=sigma_k)
+#     p_Xk = y * pi_k
+#     return p_Xk.reshape(-1, 1)
+#
+#
+# # tested
+# def _calculate_prob_X(X, all_pi, all_mu, all_sigma):
+#     """
+#     Calculates P(X), the denominator in the inference equation. Uses the following formula:
+#
+#     P(X^n) = SUM_k=1^K p(X^n|c^n=k) * p(c^n=k)
+#
+#     :param X:
+#     :param all_pi:
+#     :param all_mu:
+#     :param all_sigma:
+#     :return:
+#     """
+#     N = len(X)
+#     K = len(all_pi)
+#     all_prob = np.zeros((N, K))
+#
+#     for k in range(K):
+#         prob_Xk = _calculate_prob_Xk(X, all_pi, all_mu, all_sigma, k)
+#         all_prob[:, k] = prob_Xk[:, 0]
+#     p_X = np.sum(all_prob, axis=1).reshape(-1, 1)
+#     return p_X
+#
+#
+# # tested
+# def calculate_log_likelihood(X, all_pi, all_mu, all_sigma):
+#     p_X = _calculate_prob_X(X, all_pi, all_mu, all_sigma)
+#     log_p_X = np.log(p_X)
+#     sum_p_X = np.sum(log_p_X,axis=0)
+#     return sum_p_X.item(0)
+#
+#
+# # tested
+# def e_step(X, all_pi, all_mu, all_sigma):
+#     """
+#     Infers soft assignments to each cluster for each sample.
+#
+#     :param X:
+#     :param all_pi:
+#     :param all_mu:
+#     :param all_sigma:
+#     :return:
+#     """
+#     p_X = _calculate_prob_X(X, all_pi, all_mu, all_sigma)
+#
+#     N = len(X)
+#     K = len(all_pi)
+#     A = np.zeros((N, K))
+#
+#     for k in range(K):
+#         p_Xk = _calculate_prob_Xk(X, all_pi, all_mu, all_sigma, k)
+#         a_k = p_Xk / p_X
+#         A[:, k] = a_k[:, 0]
+#
+#     return A
 
 
 

packages/tests/test_expectation_maximization.py
@@ -13,7 +13,7 @@ def test__sum_soft_assignments():
     np.testing.assert_array_equal(actual, expected)
 
 
-def test__sum_soft_assignments2():
+def test__sum_soft_assignments__2():
     A = np.array([[0.5, 0.2, 0.3],
                   [0.2, 0.6, 0.2],
                   [0.1, 0.1, 0.8],
@@ -61,18 +61,6 @@ def test__calculate_mu_k():
         np.testing.assert_allclose(actual, expected[k], atol=1e-16)  # rounding issues
 
 
-def test__calculate_X_corr():
-    X = np.array([[6, 4],
-                  [4, 1],
-                  [1, 2],
-                  [2, 1]])
-    mu_k = np.array([4.3, 2.6])
-
-    expected = np.array([4.85, 2.65, 11.25, 7.85]).reshape(-1, 1)
-    actual = em._calculate_X_corr(X, mu_k)
-    np.testing.assert_allclose(actual, expected, atol=1e-16)
-
-
 def test__calculate_sigma_k_0():
     A = np.array([[0.5, 0.2, 0.3],
                   [0.2, 0.6, 0.2],
@@ -87,7 +75,8 @@ def test__calculate_sigma_k_0():
     mu_k = np.array([4.3, 2.6])
     k = 0
 
-    expected = 5.65
+    expected = np.array([[3.61, 2.22],
+                         [2.22, 2.04]])
     actual = em._calculate_sigma_k(X, A, S, mu_k, k)
     np.testing.assert_allclose(actual, expected, atol=1e-16)
 
@@ -106,7 +95,8 @@ def test__calculate_sigma_k_1():
     mu_k = np.array([3.9, 1.7])
     k = 1
 
-    expected = 3.5
+    expected = np.array([[2.09, 0.97],
+                         [0.97, 1.41]])
     actual = em._calculate_sigma_k(X, A, S, mu_k, k)
     np.testing.assert_allclose(actual, expected, atol=1e-16)
 
@@ -125,173 +115,174 @@ def test__calculate_sigma_k_2():
     mu_k = np.array([2.4, 1.85])
     k = 2
 
-    expected = 4.0675
+    expected = np.array([[3.04, 1.06],
+                         [1.06, 1.0275]])
     actual = em._calculate_sigma_k(X, A, S, mu_k, k)
     np.testing.assert_allclose(actual, expected, atol=1e-16)
 
-
-def test_m_step():
-    A = np.array([[0.5, 0.2, 0.3],
-                  [0.2, 0.6, 0.2],
-                  [0.1, 0.1, 0.8],
-                  [0.2, 0.1, 0.7]])
-
-    X = np.array([[6, 4],
-                  [4, 1],
-                  [1, 2],
-                  [2, 1]])
-    # pi
-    expected_pi = [0.25, 0.25, 0.5]
-
-    # mu
-    expected_mu = [np.array([4.3, 2.6]),
-                   np.array([3.9, 1.7]),
-                   np.array([2.4, 1.85])]
-
-    # sigma
-    expected_sigma = [5.65, 3.5, 4.0675]
-
-    # actual results
-    actual_pi, actual_mu, actual_sigma = em.m_step(X, A)
-
-    # confirm results match expectation
-    for k in range(3):
-        # pi
-        actual_pi_k = actual_pi[k]
-        expected_pi_k = expected_pi[k]
-        assert actual_pi_k == expected_pi_k
-
-        # mu
-        actual_mu_k = actual_mu[k]
-        expected_mu_k = expected_mu[k]
-        np.testing.assert_allclose(actual_mu_k, expected_mu_k, atol=1e-16)
-
-        # sigma
-        actual_sigma_k = actual_sigma[k]
-        expected_sigma_k = expected_sigma[k]
-        np.testing.assert_allclose(actual_sigma_k, expected_sigma_k, atol=1e-16)
-
-
-def test__calculate_prob_Xk():
-    X = np.array([[6, 4],
-                  [4, 1],
-                  [1, 2],
-                  [2, 1]])
-
-    # pi
-    all_pi = [0.25, 0.25, 0.5]
-
-    # mu
-    all_mu = [np.array([4.3, 2.6]),
-              np.array([3.9, 1.7]),
-              np.array([2.4, 1.85])]
-
-    # sigma
-    all_sigma = [5.65, 3.5, 4.0675]
-
-    k = 0
-    expected = np.array([[0.0045847],
-                         [0.00557011],
-                         [0.00260219],
-                         [0.0035157]])
-    actual = em._calculate_prob_Xk(X, all_pi, all_mu, all_sigma, k)
-    np.testing.assert_allclose(actual, expected, atol=1e-7)
-
-
-def test___calculate_prob_X():
-    X = np.array([[6, 4],
-                  [4, 1],
-                  [1, 2],
-                  [2, 1]])
-
-    # pi
-    all_pi = [0.25, 0.25, 0.5]
-
-    # mu
-    all_mu = [np.array([4.3, 2.6]),
-              np.array([3.9, 1.7]),
-              np.array([2.4, 1.85])]
-
-    # sigma
-    all_sigma = [5.65, 3.5, 4.0675]
-
-    # calculate expected probabilities
-    all_prob = np.array([[0.0045847, 0.0028437, 0.00225323],
-                         [0.00557011, 0.01058452, 0.01306843],
-                         [0.00260219, 0.00337547, 0.01533292],
-                         [0.0035157, 0.00632877, 0.01755293]])
-
-    expected = np.sum(all_prob, axis=1).reshape(-1, 1)
-    actual = em._calculate_prob_X(X, all_pi, all_mu, all_sigma)
-    np.testing.assert_allclose(actual, expected, atol=1e-7)
-
-
-def test_calculate_log_likelihood():
-    X = np.array([[6, 4],
-                  [4, 1],
-                  [1, 2],
-                  [2, 1]])
-
-    # pi
-    all_pi = [0.25, 0.25, 0.5]
-
-    # mu
-    all_mu = [np.array([4.3, 2.6]),
-              np.array([3.9, 1.7]),
-              np.array([2.4, 1.85])]
-
-    # sigma
-    all_sigma = [5.65, 3.5, 4.0675]
-
-    # calculate expected probabilities
-    all_prob = np.array([[0.0045847, 0.0028437, 0.00225323],
-                         [0.00557011, 0.01058452, 0.01306843],
-                         [0.00260219, 0.00337547, 0.01533292],
-                         [0.0035157, 0.00632877, 0.01755293]])
-
-    p_X = np.sum(all_prob, axis=1).reshape(-1, 1)
-    log_p_X = np.log(p_X)
-    expected = np.sum(log_p_X, axis=0).item(0)
-    actual = em.calculate_log_likelihood(X, all_pi, all_mu, all_sigma)
-    np.testing.assert_allclose(actual, expected, atol=1e-7)
-
-
-def test_e_step():
-    X = np.array([[6, 4],
-                  [4, 1],
-                  [1, 2],
-                  [2, 1]])
-
-    # pi
-    all_pi = [0.25, 0.25, 0.5]
-
-    # mu
-    all_mu = [np.array([4.3, 2.6]),
-              np.array([3.9, 1.7]),
-              np.array([2.4, 1.85])]
-
-    # sigma
-    all_sigma = [5.65, 3.5, 4.0675]
-
-    # calculate expected probabilities
-    all_prob = np.array([[0.0045847, 0.0028437, 0.00225323],
-                         [0.00557011, 0.01058452, 0.01306843],
-                         [0.00260219, 0.00337547, 0.01533292],
-                         [0.0035157, 0.00632877, 0.01755293]])
-
-    p_X = np.sum(all_prob, axis=1).reshape(-1, 1)
-
-    expected_column_totals = np.array([[1.0],
-                                       [1.0],
-                                       [1.0],
-                                       [1.0]])
-    expected = np.array([[0.47354647, 0.29372146, 0.23273207],
-                         [0.19060674, 0.3621975, 0.44719575],
-                         [0.12210782, 0.15839415, 0.71949803],
-                         [0.12832233, 0.23099911, 0.64067856]])
-
-    actual = em.e_step(X, all_pi, all_mu, all_sigma)
-    np.testing.assert_allclose(actual, expected, atol=1e-16)
-
-    # ensure each row sums to 1.0
-    np.testing.assert_allclose(np.sum(actual, axis=1).reshape(-1, 1), expected_column_totals, atol=1e-16)
+#
+# def test_m_step():
+#     A = np.array([[0.5, 0.2, 0.3],
+#                   [0.2, 0.6, 0.2],
+#                   [0.1, 0.1, 0.8],
+#                   [0.2, 0.1, 0.7]])
+#
+#     X = np.array([[6, 4],
+#                   [4, 1],
+#                   [1, 2],
+#                   [2, 1]])
+#     # pi
+#     expected_pi = [0.25, 0.25, 0.5]
+#
+#     # mu
+#     expected_mu = [np.array([4.3, 2.6]),
+#                    np.array([3.9, 1.7]),
+#                    np.array([2.4, 1.85])]
+#
+#     # sigma
+#     expected_sigma = [5.65, 3.5, 4.0675]
+#
+#     # actual results
+#     actual_pi, actual_mu, actual_sigma = em.m_step(X, A)
+#
+#     # confirm results match expectation
+#     for k in range(3):
+#         # pi
+#         actual_pi_k = actual_pi[k]
+#         expected_pi_k = expected_pi[k]
+#         assert actual_pi_k == expected_pi_k
+#
+#         # mu
+#         actual_mu_k = actual_mu[k]
+#         expected_mu_k = expected_mu[k]
+#         np.testing.assert_allclose(actual_mu_k, expected_mu_k, atol=1e-16)
+#
+#         # sigma
+#         actual_sigma_k = actual_sigma[k]
+#         expected_sigma_k = expected_sigma[k]
+#         np.testing.assert_allclose(actual_sigma_k, expected_sigma_k, atol=1e-16)
+#
+#
+# def test__calculate_prob_Xk():
+#     X = np.array([[6, 4],
+#                   [4, 1],
+#                   [1, 2],
+#                   [2, 1]])
+#
+#     # pi
+#     all_pi = [0.25, 0.25, 0.5]
+#
+#     # mu
+#     all_mu = [np.array([4.3, 2.6]),
+#               np.array([3.9, 1.7]),
+#               np.array([2.4, 1.85])]
+#
+#     # sigma
+#     all_sigma = [5.65, 3.5, 4.0675]
+#
+#     k = 0
+#     expected = np.array([[0.0045847],
+#                          [0.00557011],
+#                          [0.00260219],
+#                          [0.0035157]])
+#     actual = em._calculate_prob_Xk(X, all_pi, all_mu, all_sigma, k)
+#     np.testing.assert_allclose(actual, expected, atol=1e-7)
+#
+#
+# def test___calculate_prob_X():
+#     X = np.array([[6, 4],
+#                   [4, 1],
+#                   [1, 2],
+#                   [2, 1]])
+#
+#     # pi
+#     all_pi = [0.25, 0.25, 0.5]
+#
+#     # mu
+#     all_mu = [np.array([4.3, 2.6]),
+#               np.array([3.9, 1.7]),
+#               np.array([2.4, 1.85])]
+#
+#     # sigma
+#     all_sigma = [5.65, 3.5, 4.0675]
+#
+#     # calculate expected probabilities
+#     all_prob = np.array([[0.0045847, 0.0028437, 0.00225323],
+#                          [0.00557011, 0.01058452, 0.01306843],
+#                          [0.00260219, 0.00337547, 0.01533292],
+#                          [0.0035157, 0.00632877, 0.01755293]])
+#
+#     expected = np.sum(all_prob, axis=1).reshape(-1, 1)
+#     actual = em._calculate_prob_X(X, all_pi, all_mu, all_sigma)
+#     np.testing.assert_allclose(actual, expected, atol=1e-7)
+#
+#
+# def test_calculate_log_likelihood():
+#     X = np.array([[6, 4],
+#                   [4, 1],
+#                   [1, 2],
+#                   [2, 1]])
+#
+#     # pi
+#     all_pi = [0.25, 0.25, 0.5]
+#
+#     # mu
+#     all_mu = [np.array([4.3, 2.6]),
+#               np.array([3.9, 1.7]),
+#               np.array([2.4, 1.85])]
+#
+#     # sigma
+#     all_sigma = [5.65, 3.5, 4.0675]
+#
+#     # calculate expected probabilities
+#     all_prob = np.array([[0.0045847, 0.0028437, 0.00225323],
+#                          [0.00557011, 0.01058452, 0.01306843],
+#                          [0.00260219, 0.00337547, 0.01533292],
+#                          [0.0035157, 0.00632877, 0.01755293]])
+#
+#     p_X = np.sum(all_prob, axis=1).reshape(-1, 1)
+#     log_p_X = np.log(p_X)
+#     expected = np.sum(log_p_X, axis=0).item(0)
+#     actual = em.calculate_log_likelihood(X, all_pi, all_mu, all_sigma)
+#     np.testing.assert_allclose(actual, expected, atol=1e-7)
+#
+#
+# def test_e_step():
+#     X = np.array([[6, 4],
+#                   [4, 1],
+#                   [1, 2],
+#                   [2, 1]])
+#
+#     # pi
+#     all_pi = [0.25, 0.25, 0.5]
+#
+#     # mu
+#     all_mu = [np.array([4.3, 2.6]),
+#               np.array([3.9, 1.7]),
+#               np.array([2.4, 1.85])]
+#
+#     # sigma
+#     all_sigma = [5.65, 3.5, 4.0675]
+#
+#     # calculate expected probabilities
+#     all_prob = np.array([[0.0045847, 0.0028437, 0.00225323],
+#                          [0.00557011, 0.01058452, 0.01306843],
+#                          [0.00260219, 0.00337547, 0.01533292],
+#                          [0.0035157, 0.00632877, 0.01755293]])
+#
+#     p_X = np.sum(all_prob, axis=1).reshape(-1, 1)
+#
+#     expected_column_totals = np.array([[1.0],
+#                                        [1.0],
+#                                        [1.0],
+#                                        [1.0]])
+#     expected = np.array([[0.47354647, 0.29372146, 0.23273207],
+#                          [0.19060674, 0.3621975, 0.44719575],
+#                          [0.12210782, 0.15839415, 0.71949803],
+#                          [0.12832233, 0.23099911, 0.64067856]])
+#
+#     actual = em.e_step(X, all_pi, all_mu, all_sigma)
+#     np.testing.assert_allclose(actual, expected, atol=1e-16)
+#
+#     # ensure each row sums to 1.0
+#     np.testing.assert_allclose(np.sum(actual, axis=1).reshape(-1, 1), expected_column_totals, atol=1e-16)