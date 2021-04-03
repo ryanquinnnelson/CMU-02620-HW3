9b19ff96eec87c82f053e2ca3aa4369181288e28
ryanquinnnelson <ryan.q.nelson@gmail.com>
2021-04-02 23:01:59 -0400

    Added ability to initialize parameters

--- packages/mixture/GaussianMixture.py
+++ packages/mixture/GaussianMixture.py
@@ -6,10 +6,10 @@ from packages.mixture import expectation_maximization as em
 
 
 # tested
-def _initialize_assignments(X, K):
+def _get_random_assignments(X, K):
     """
-    Sets initial soft-assignments for all samples for all clusters.
-    Ensures the soft-assignments for each cluster add up to 1.0 for a given sample.
+    Sets initial soft-assignments for all samples for all clusters using random values drawn from a uniform
+    distribution. Standardizes soft-assignments for each cluster so assignments add up to 1.0 for a given sample.
 
     :param X: N x J matrix, where N is the number of samples and J is the number of features per sample.
     :param K: int, number of clusters in model.
@@ -27,8 +27,71 @@ def _initialize_assignments(X, K):
     # scale values derived so every column adds up to 1.0
     sum_A_T = np.sum(A_T, axis=0)
     A_T_scaled = A_T / sum_A_T
+    A = A_T_scaled.T
+    return A
 
-    return A_T_scaled.T
+
+# tested
+def _use_initial_assignments(X, K, A_init):
+    """
+    Sets initial soft-assignments for all samples for all clusters using given assignments.
+    Raises ValueError if A_init is not K x 1 or N x K.
+
+    :param X: N x J matrix, where N is the number of samples and J is the number of features per sample.
+    :param K: int, number of clusters in model.
+    :param A_init: K x 1 array or N X K matrix of initial soft assignments.
+    :return: N x K matrix, where N is the number of samples and K is the number of clusters.
+             Represents the soft assignments for each cluster for all samples.
+             A[n][k] is the soft-assignment of the nth sample for the kth cluster.
+    """
+    N = len(X)
+    if A_init.shape == (K,):  # A is an 1 x K array, assign same values to every sample
+
+        A = np.zeros((N, K))
+        A[:] = A_init
+
+    elif A_init.shape == (N, K):
+        A = A_init  # A is an N x K matrix, use it directly
+    else:
+        raise ValueError('Initial soft assignments matrix must be provided as a K x 1 array or N x K matrix.', A_init)
+
+    return A
+
+
+# tested
+def _check_assignments(A):
+    """
+    Confirms each row of soft assignments adds up to 1. Raises AssertionError if any row does not.
+
+    :param A: N x K matrix, where N is the number of samples and K is the number of clusters.
+             Represents the soft assignments for each cluster for all samples.
+             A[n][k] is the soft-assignment of the nth sample for the kth cluster.
+    :return: None
+    """
+    expected = np.ones((len(A),))
+    np.testing.assert_allclose(np.sum(A, axis=1), expected, atol=1e-16)
+
+
+# tested
+def _initialize_assignments(X, K, A):
+    """
+    Sets initial soft-assignments for all samples for all clusters.
+    Ensures the soft-assignments for each cluster add up to 1.0 for a given sample.
+
+    :param X: N x J matrix, where N is the number of samples and J is the number of features per sample.
+    :param K: int, number of clusters in model.
+    :return: N x K matrix, where N is the number of samples and K is the number of clusters.
+            Represents the soft assignments for each cluster for all samples.
+            A[n][k] is the soft-assignment of the nth sample for the kth cluster.
+    """
+
+    if A is None:  # an initial assignment is not provided
+        A = _get_random_assignments(X, K)
+    else:
+        A = _use_initial_assignments(X, K, A)
+
+    _check_assignments(A)
+    return A
 
 
 # def _assign_clusters(A):
@@ -37,35 +100,60 @@ def _initialize_assignments(X, K):
 #     return predictions
 
 
+# tested
+def _initialize_parameters(X, A, pi_init, mu_init, sigma_init):
+    # calculate initial values based on A
+    all_pi, all_mu, all_sigma = em.m_step(X, A)
+
+    if pi_init is not None:
+        all_pi = pi_init
+
+    if mu_init is not None:
+        all_mu = mu_init
+
+    if sigma_init is not None:
+        all_sigma = sigma_init
+
+    return all_pi, all_mu, all_sigma
+
+
 class GaussianMixture:
 
-    def __init__(self, K, epsilon):
+    def __init__(self, K, epsilon, pi_init=None, mu_init=None, sigma_init=None, A_init=None):
         """
+        Initializes Gaussian Mixture model with hyperparameters and also allows initial parameters to be passed in.
 
         :param K: int, number of clusters in model.
         :param epsilon: float, difference threshold for convergence calculation. Convergence is defined as the point
-        at which the difference in the objective function between iterations is less than epsilon.
+                        at which the difference in the objective function between iterations is less than epsilon.
+        :param pi_init:
+        :param mu_init:
+        :param sigma_init:
+        :param A_init:
         """
         self.K = K
         self.epsilon = epsilon
+        self.pi_init = pi_init
+        self.mu_init = mu_init
+        self.sigma_init = sigma_init
+        self.A_init = A_init
         self.pi = None
         self.mu = None
         self.sigma = None
 
-    def fit_and_score(self,X):
+    def fit_and_score(self, X):
+
+        # initialization
         scores = []
+        A = _initialize_assignments(X, self.K, self.A_init)
+        all_pi, all_mu, all_sigma = _initialize_parameters(X, A, self.pi_init, self.mu_init, self.sigma_init)
 
-        A = _initialize_assignments(X, self.K)
-
-        # m step - estimate parameters
-        all_pi, all_mu, all_sigma = em.m_step(X, A)
-
-        # calculate difference between previous and current objective values
+        # calculate first objective value
         prev_objective = em.calculate_log_likelihood(X, all_pi, all_mu, all_sigma)
         scores.append(prev_objective)
 
-        diff = self.epsilon  # to enter the while loop
         counter = 0
+        diff = self.epsilon  # to enter the while loop
         while diff >= self.epsilon:
 
             # e step - update assignments
@@ -77,13 +165,14 @@ class GaussianMixture:
             # calculate difference between previous and current objective values
             curr_objective = em.calculate_log_likelihood(X, all_pi, all_mu, all_sigma)
             scores.append(curr_objective)
-            diff = np.abs(curr_objective - prev_objective)
 
             # take actions depending on whether there will be another round
+            diff = np.abs(curr_objective - prev_objective)
             if diff >= self.epsilon:  # there will be another iteration
                 prev_objective = curr_objective  # save for next round
                 counter += 1
 
+            # determine if there is an infinite loop
             if counter > 10000:
                 raise ValueError('Model took too many iterations to converge:', counter)  # something is wrong
 
@@ -92,7 +181,7 @@ class GaussianMixture:
         self.mu = all_mu
         self.sigma = all_sigma
 
-        print(counter)  # for fun, see how many iterations it took to converge
+        print(counter)  # for fun, number of iterations it took to converge
         return scores
 
     def fit(self, X):
--- packages/tests/test_GaussianMixture.py
+++ packages/tests/test_GaussianMixture.py
@@ -1,16 +1,15 @@
 import numpy as np
 from packages.mixture import GaussianMixture as gm
+import pytest
 
 
-def test__initialize_assignments():
+def test__get_random_assignments():
     X = np.array([[6, 4],
                   [4, 1],
                   [1, 2],
                   [2, 1]])
-
     K = 3
-
-    actual = gm._initialize_assignments(X, K)
+    actual = gm._get_random_assignments(X, K)
 
     # ensure each row adds up to 1.0
     expected_column_totals = np.array([[1.0],
@@ -21,7 +20,143 @@ def test__initialize_assignments():
     assert actual.shape == (4, 3)
 
 
-def test___init__():
+def test__initialize_assignments__array_version():
+    X = np.array([[6, 4],
+                  [4, 1],
+                  [1, 2],
+                  [2, 1]])
+    K = 3
+
+    A_init = np.array([0.3, 0.3, 0.4])
+    expected = np.array([[0.3, 0.3, 0.4],
+                         [0.3, 0.3, 0.4],
+                         [0.3, 0.3, 0.4],
+                         [0.3, 0.3, 0.4]])
+    actual = gm._use_initial_assignments(X, K, A_init)
+    np.testing.assert_array_equal(actual, expected)
+
+
+def test__initialize_assignments__matrix_version():
+    X = np.array([[6, 4],
+                  [4, 1],
+                  [1, 2],
+                  [2, 1]])
+    K = 3
+
+    A_init = np.array([[0.3, 0.1, 0.6],
+                       [0.3, 0.5, 0.2],
+                       [0.3, 0.3, 0.4],
+                       [0.1, 0.1, 0.8]])
+
+    actual = gm._use_initial_assignments(X, K, A_init)
+    np.testing.assert_array_equal(actual, A_init)
+
+
+def test__initialize_assignments__error_version():
+    X = np.array([[6, 4],
+                  [4, 1],
+                  [1, 2],
+                  [2, 1]])
+    K = 3
+
+    A_init = np.array([[0.3, 0.1, 0.6],
+                       [0.3, 0.5, 0.2],
+                       [0.3, 0.3, 0.4]])
+
+    with pytest.raises(ValueError):
+        gm._use_initial_assignments(X, K, A_init)
+
+
+def test__check_assignments__success():
+    A = np.array([[0.3, 0.1, 0.6],
+                  [0.3, 0.5, 0.2],
+                  [0.3, 0.3, 0.4],
+                  [0.1, 0.1, 0.8]])
+
+    gm._check_assignments(A)
+
+
+def test__check_assignments__failure():
+    A = np.array([[0.3, 0.1, 0.1],
+                  [0.3, 0.5, 0.1],
+                  [0.3, 0.3, 0.4],
+                  [0.1, 0.1, 0.8]])
+
+    with pytest.raises(AssertionError):
+        gm._check_assignments(A)
+
+
+def test__initialize_assignments__no_initial_assignment():
+    X = np.array([[6, 4],
+                  [4, 1],
+                  [1, 2],
+                  [2, 1]])
+
+    K = 3
+    A_init = None
+
+    actual = gm._initialize_assignments(X, K, A_init)
+    assert actual.shape == (4, 3)
+
+
+def test__initialize_assignments__initial_assignment():
+    X = np.array([[6, 4],
+                  [4, 1],
+                  [1, 2],
+                  [2, 1]])
+
+    K = 3
+    A_init = np.array([0.3, 0.3, 0.4])
+    expected = np.array([[0.3, 0.3, 0.4],
+                         [0.3, 0.3, 0.4],
+                         [0.3, 0.3, 0.4],
+                         [0.3, 0.3, 0.4]])
+
+    actual = gm._initialize_assignments(X, K, A_init)
+    np.testing.assert_array_equal(actual, expected)
+
+
+def test__initialize_parameters__no_initial_values():
+    X = np.array([[6, 4],
+                  [4, 1],
+                  [1, 2],
+                  [2, 1]])
+
+    A = np.array([[0.3, 0.3, 0.4],
+                  [0.3, 0.3, 0.4],
+                  [0.3, 0.3, 0.4],
+                  [0.3, 0.3, 0.4]])
+    pi_init = None
+    mu_init = None
+    sigma_init = None
+
+    actual_pi, actual_mu, actual_sigma = gm._initialize_parameters(X, A, pi_init, mu_init, sigma_init)
+    assert len(actual_pi) == 3
+    assert len(actual_mu) == 3
+    assert len(actual_sigma) == 3
+
+
+def test__initialize_parameters__all_initial_values():
+    X = np.array([[6, 4],
+                  [4, 1],
+                  [1, 2],
+                  [2, 1]])
+
+    A = np.array([[0.3, 0.3, 0.4],
+                  [0.3, 0.3, 0.4],
+                  [0.3, 0.3, 0.4],
+                  [0.3, 0.3, 0.4]])
+    pi_init = [0.5, 1.0, 3.0]
+    mu_init = [0.5, 1.0, 3.0]
+    sigma_init = [0.5, 1.0, 3.0]
+
+    actual_pi, actual_mu, actual_sigma = gm._initialize_parameters(X, A, pi_init, mu_init, sigma_init)
+    assert actual_pi == pi_init
+    assert actual_mu == mu_init
+    assert actual_sigma == sigma_init
+
+
+def test___init__no_parameters():
     K = 3
     epsilon = 1e-3
 
@@ -29,6 +164,31 @@ def test___init__():
 
     assert model.K == K
     assert model.epsilon == epsilon
+    assert model.pi_init is None
+    assert model.mu_init is None
+    assert model.sigma_init is None
+    assert model.A_init is None
+    assert model.pi is None
+    assert model.mu is None
+    assert model.sigma is None
+
+
+def test__init__all_parameters():
+    K = 3
+    epsilon = 1e-3
+    pi_init = [1, 2, 3]
+    mu_init = [4, 5, 6]
+    sigma_init = [7, 8, 9]
+    A_init = [10, 11, 12]
+
+    model = gm.GaussianMixture(K, epsilon, pi_init, mu_init, sigma_init, A_init)
+
+    assert model.K == K
+    assert model.epsilon == epsilon
+    assert model.pi_init == pi_init
+    assert model.mu_init == mu_init
+    assert model.sigma_init == sigma_init
+    assert model.A_init == A_init
     assert model.pi is None
     assert model.mu is None
     assert model.sigma is None
